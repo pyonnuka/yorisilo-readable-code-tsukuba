99d62c3b03d8a801d1431fa6e715dc9acd68454d
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-04 15:02:17 +0900

    spec8: ユーザを指定できるように実装

--- recipe.rb
+++ recipe.rb
@@ -1,11 +1,16 @@
 # coding: utf-8
 
+# レシピのユーザー名、レシピ名、作り方を配列で返す
+def getRecipeDetail(recipe)
+  recipe.split(" ")
+end
+
 def showRecipesId(recipes)
   File.open(recipes) do |f|
     ary = f.readlines
     ary.each.with_index(1) do |name, i|
-      idName = "#{i}: #{name}"
-      puts idName
+      recipeDetail = getRecipeDetail(name)
+      puts "#{i}: #{recipeDetail[1]} #{recipeDetail[2]}" if recipeDetail[0] == ARGV[0]
     end
   end
 end
@@ -13,14 +18,15 @@ end
 def recipeId(recipes, id)
   File.open(recipes) do |f|
     ary = f.readlines
-    idName = ary[id-1]
-    puts "#{id}: #{idName}" if id != 0
+    recipeDetail = getRecipeDetail(ary[id-1])
+    puts "#{id}: #{recipeDetail[1]} #{recipeDetail[2]}" if id != 0 && recipeDetail[0] == ARGV[0]
     #showRecipesId(recipes)  if id == 0
   end
 end
 
-if ARGV.size == 1
-  showRecipesId(ARGV[0])
-elsif ARGV.size == 2
-  recipeId(ARGV[0], ARGV[1].to_i)
+puts "#{ARGV[0]}:"
+if ARGV.size == 2
+  showRecipesId(ARGV[1])
+elsif ARGV.size == 3
+  recipeId(ARGV[1], ARGV[2].to_i)
 end
